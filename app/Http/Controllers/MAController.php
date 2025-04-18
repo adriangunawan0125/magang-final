ef34927f03b90c6b36c8d8f323c228f21250bc0f
NabilaEky <149370826+NabilaEky@users.noreply.github.com>
2025-04-18 17:25:41 +0700

    Update MAController.php

--- app/Http/Controllers/MAController.php
+++ app/Http/Controllers/MAController.php
@@ -7,28 +7,27 @@ use App\Models\Guru;
 use App\Models\Sosmed_ma;
 use App\Models\Kegiatan_ma;
 use App\Models\Carousel_ma;
-use App\Models\sambutan_ma;
-use App\Models\Berita; 
-
+use App\Models\Sambutan_ma;
+use App\Models\Berita;
 
 class MAController extends Controller
 {
-   
     public function index()
     {
-        $sambutan = sambutan_ma::first();
+        $sambutan = Sambutan_ma::first();
         $carousels_ma = Carousel_ma::all();
         $guru = Guru::all(); 
         $sosmed = Sosmed_ma::all();
         $kegiatans = Kegiatan_ma::all();
-<<<<<<< HEAD
-        $berita = Berita::latest()->get(); // <-- tambahkan ini
+        $berita = Berita::latest()->get();
     
-        return view('MA.MA', compact('guru', 'sosmed', 'kegiatans', 'carousels', 'sambutan', 'berita'));
-=======
-
-        return view('MA.MA', compact('guru', 'sosmed', 'kegiatans', 'carousels_ma', 'sambutan'));
->>>>>>> d5edc109fd6c9b56241f842177d397a2a7cac4f5
+        return view('MA.MA', compact(
+            'guru', 
+            'sosmed', 
+            'kegiatans', 
+            'carousels_ma', 
+            'sambutan',
+            'berita'
+        ));
     }
-    
 }
